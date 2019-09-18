7680f1ba0cb1f4ab2e94e4700c2857c7765a7d3c
Abraham Williams <4braham@gmail.com>
2019-09-17 21:29:04 -0500

    Move to webpack build

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use:  [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      files: {
+        css: [ './index.css' ],
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false,
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: 'src/img',
+        to: 'img',
+      },
+      {
+        from: 'src/404.html',
+        to: '404.html',
+      },
+      {
+        from: 'src/manifest.json',
+        to: 'manifest.json',
+      },
+    ]),
+  ],
+};
